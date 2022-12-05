b116dfc24fbae0de18749dc1c7727d4e51e82fb3
dttaivlo1 <dttaivlo@gmail.com>
2022-12-06 00:50:17 +0700

    a

--- client/app/constants/index.js
+++ client/app/constants/index.js
@@ -2,7 +2,7 @@ export const BASE_API_URL = process.env.BASE_API_URL;
 
 export const SOCKET_URL =
   window.location.host.indexOf('localhost') >= 0
-    ? 'http://127.0.0.1:3000'
+    ? 'https://127.0.0.1:3000'
     : window.location.host;
 
 export const ROLE_ADMIN = 'ROLE_ADMIN';
